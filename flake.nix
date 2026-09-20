78dae0bbdaf12d43156516a3e5d3cfd077d4e848
batroni <dein@email.com>
2026-09-20 20:48:41 +0200

    fix: correct typo in outputs for noctalia in flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
   };
 
   
-  outputs = { self, nixpkgs, home-manager, serpantinum, nuctulia, ... }:
+  outputs = { self, nixpkgs, home-manager, serpantinum, noctalia, ... }:
     let
       system = "x86_64-linux";
     in
